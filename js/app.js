bd48732154f69f67ae84e9ec52d85783552e32d9
pgap22 <gerardo.saz120@gmail.com{>
2022-05-09 14:31:31 -0600

    js no optimized

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
-let e=document.querySelector(".header__menu"),t=document.querySelector(".nav"),i=document.querySelector(".nav__closeMenu"),l=document.querySelector(".header__blackScreen");function n(){t.style.transform="translateX(0%)",l.style.display="block",e.style.visibility="hidden",i.style.visibility="visible"}function s(){t.style.transform="translateX(-100%)",l.style.display="none",e.style.opacity=1,e.style.display="block",i.style.visibility="hidden",e.style.visibility="visible"}i.style.visibility="hidden",e.addEventListener("click",(()=>{n()})),i.addEventListener("click",(()=>{s()}));document.querySelector(".header__logo");window.addEventListener("resize",(()=>{console.log(),window.visualViewport.width>=769?(n(),e.style.display="none",l.style.display="none",t.style.transition="none"):(s(),setTimeout((()=>{t.style.transition="ease-in 200ms"}),100))}));
+//Hamburguer Menu Responsive Mobile
+
+let hamburguerMenu = document.querySelector(".header__menu");
+let menuNav = document.querySelector(".nav");
+let closeHamburguer = document.querySelector(".nav__closeMenu");
+let blackScreen = document.querySelector(".header__blackScreen")
+closeHamburguer.style.visibility = "hidden";
+
+function showMenu(){
+    menuNav.style.transform = "translateX(0%)";
+    blackScreen.style.display = "block";
+    hamburguerMenu.style.visibility = "hidden";
+    closeHamburguer.style.visibility = "visible";
+}
+function quitMenu(){
+    menuNav.style.transform = "translateX(-100%)";
+    blackScreen.style.display = "none";
+    hamburguerMenu.style.opacity = 1;
+    hamburguerMenu.style.display = "block";
+    closeHamburguer.style.visibility = "hidden";
+    hamburguerMenu.style.visibility = "visible";    
+}
+
+hamburguerMenu.addEventListener("click", ()=>{
+showMenu()
+
+})
+closeHamburguer.addEventListener("click", ()=>{
+quitMenu()
+})
+
+//Tablet BreakPoint
+let icono = document.querySelector(".header__logo")
+
+window.addEventListener("resize", ()=>{
+    console.log();
+    (window.visualViewport.width >=769) ? showMenu() & (hamburguerMenu.style.display = "none" ) & (blackScreen.style.display = "none") & (menuNav.style.transition = "none") : quitMenu() & (setTimeout(() => {(menuNav.style.transition = "ease-in 200ms")}, 100));
+})
