1964584c9e0f4630e67352c9dc9095a0503dd4e4
pgap22 <gerardo.saz120@gmail.com{>
2022-05-09 00:41:33 -0600

    desktop/table completed

--- js/app.js
+++ js/app.js
@@ -6,19 +6,33 @@ let closeHamburguer = document.querySelector(".nav__closeMenu");
 let blackScreen = document.querySelector(".header__blackScreen")
 closeHamburguer.style.visibility = "hidden";
 
-
-hamburguerMenu.addEventListener("click", ()=>{
+function showMenu(){
     menuNav.style.transform = "translateX(0%)";
     blackScreen.style.display = "block";
     hamburguerMenu.style.visibility = "hidden";
     closeHamburguer.style.visibility = "visible";
-
-})
-closeHamburguer.addEventListener("click", ()=>{
+}
+function quitMenu(){
     menuNav.style.transform = "translateX(-100%)";
     blackScreen.style.display = "none";
     hamburguerMenu.style.opacity = 1;
     hamburguerMenu.style.display = "block";
     closeHamburguer.style.visibility = "hidden";
     hamburguerMenu.style.visibility = "visible";    
+}
+
+hamburguerMenu.addEventListener("click", ()=>{
+showMenu()
+
+})
+closeHamburguer.addEventListener("click", ()=>{
+quitMenu()
+})
+
+//Tablet BreakPoint
+let icono = document.querySelector(".header__logo")
+
+window.addEventListener("resize", ()=>{
+    console.log();
+    (window.visualViewport.width >=769) ? showMenu() & (hamburguerMenu.style.display = "none" ) & (blackScreen.style.display = "none") & (menuNav.style.transition = "none") : quitMenu() & (setTimeout(() => {(menuNav.style.transition = "ease-in 200ms")}, 100));
 })
