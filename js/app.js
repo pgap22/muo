441aeebe8189e9e30eaf94b334e4bd37b886f023
pgap22 <gerardo.saz120@gmail.com{>
2022-05-08 20:35:25 -0600

    Mobile Completed

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//Hamburguer Menu Responsive Mobile
+
+let hamburguerMenu = document.querySelector(".header__menu");
+let menuNav = document.querySelector(".nav");
+let closeHamburguer = document.querySelector(".nav__closeMenu");
+let blackScreen = document.querySelector(".header__blackScreen")
+closeHamburguer.style.visibility = "hidden";
+
+
+hamburguerMenu.addEventListener("click", ()=>{
+    menuNav.style.transform = "translateX(0%)";
+    blackScreen.style.display = "block";
+    hamburguerMenu.style.visibility = "hidden";
+    closeHamburguer.style.visibility = "visible";
+
+})
+closeHamburguer.addEventListener("click", ()=>{
+    menuNav.style.transform = "translateX(-100%)";
+    blackScreen.style.display = "none";
+    hamburguerMenu.style.opacity = 1;
+    hamburguerMenu.style.display = "block";
+    closeHamburguer.style.visibility = "hidden";
+    hamburguerMenu.style.visibility = "visible";    
+})
